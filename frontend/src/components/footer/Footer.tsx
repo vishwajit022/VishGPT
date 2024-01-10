97fc5db59c316db380a514be750fd22822af5662
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-01-10 06:34:00 +0530

    cors setup join

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built with ChatGPT API 🤪
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
